32740b9fc9edf4a15b1437c0a008b08b45146a86
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-04-19 17:05:28 +0500

    modify cors.rb to handle cross origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://fast-lane-racers.netlify.app/"
+    origins "https://fast-lane-racers.netlify.app"
 
     resource "*",
       headers: :any,
