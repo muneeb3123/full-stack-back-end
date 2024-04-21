d717ac13f917d6db6f4c454dc9c9667c87e422b4
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-04-21 20:53:01 +0500

    modify

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,9 +2,21 @@ class ReservationsController < ApplicationController
     before_action :authenticate_user!, only: [:create]
   
     def index
-      @reservation = current_user.reservations.order(created_at: :desc)
-      render json: @reservation
-    end
+        @reservations = current_user.reservations.order(created_at: :desc)
+      
+        car_info = @reservations.map do |reservation|
+          car = Car.find(reservation.car_id)
+          { name: car.name, image: car.image }
+        end
+      
+        merged_data = @reservations.map.with_index do |reservation, index|
+          reservation.attributes.merge(car_info[index])
+        end
+      
+        render json: merged_data
+      end
+      
+      
   
     def create
       @reserve = current_user.reservations.new(reservation_params)
